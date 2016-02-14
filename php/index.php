50371674e23899ba937b51b9ea741bb8ef4008c1
AhmedSobhy <eng_ahmed9393@yahoo.com>
2016-02-14 22:46:14 +0200

    a

--- php/index.php
+++ php/index.php
@@ -1,21 +1,2 @@
+ <?php echo '<h1>Hello World</h1>'; ?>
 
-
-
-
-
-
-
-
-
-<html>
-<head>
-        <title>Hello World!</title>
-    </head>
-    <body>
-
-<p>welcome</p>
-        <?php echo '<h1>Hello World</h1>'; ?>
-    </body>
-
-
-</html>
